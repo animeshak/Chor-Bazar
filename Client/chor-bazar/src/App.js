3f8395ffad516d7beaf8b96b7c9fd1fb55d1b2af
Animesh Kambhale <animeshkambhale2001@gmail.com>
2023-08-23 14:12:23 +0530

    initial push for server and react app

---
+++ Client/chor-bazar/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Productlisting from './Components/Productlisting';
+
+function App() {
+  return (
+    <>
+    <Productlisting/>
+    </>
+  );
+}
+
+export default App;
